96fab982ba8582bab603da63bf59a2ebcda6294d
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-06-16 10:21:21 +0900

    init

---
+++ src/components/MyButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :style="styleObject" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "blue",
+    },
+    size: {
+      type: String,
+      default: "medium",
+    },
+  },
+  computed: {
+    styleObject() {
+      return {
+        color: this.color,
+        fontSize: this.size === "large" ? "24px" : "16px",
+      };
+    },
+  },
+  methods: {
+    onClick(event) {
+      this.$emit("click", event);
+    },
+  },
+};
+</script>
